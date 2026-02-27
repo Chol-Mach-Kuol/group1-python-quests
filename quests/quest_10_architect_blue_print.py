43727326e1f15976361a842b729b2db50842f227
OurFirstService <ourfirstservice01@gmail.com>
2026-02-27 11:37:34 +0200

    Add quests 10, 15, 20, 23, and 27 to the completed work

---
+++ quests/quest_10_architect_blue_print.py
@@ -0,0 +1,5 @@
+length=float(input("Enter the length of the rectangle (decimal allowed): "))
+width=float(input("Enter the width of the rectangle (decimal allowed): "))
+area=length*width
+
+print("The area of the rectangle is: ", area)
